7ed0ec532aa25abd144ee92db1b65028d4051c76
Artur May <artur.may@ua-gis.com>
2022-08-18 21:52:02 +0300

    - логування юзерів v3 мейлоси працюють!!))

---
+++ src/main/java/com/onlinestore/ethnonamysto/security/config/MailConfig.java
@@ -0,0 +1,38 @@
+package com.onlinestore.ethnonamysto.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+/**
+ * Created by Artur May
+ * Date 14.08.2022
+ * Time 14:32
+ */
+@Configuration
+public class MailConfig {
+
+    @Bean
+    public JavaMailSender javaMailService() {
+        JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+
+        javaMailSender.setProtocol("smtp");
+        javaMailSender.setHost("smtp.gmail.com");
+        javaMailSender.setPort(587);
+        javaMailSender.setUsername("etno.namysto@gmail.com");
+        javaMailSender.setPassword("juoehfqkclvrpajj");
+
+        Properties properties = new Properties();
+        properties.setProperty("mail.smtp.auth", "true");
+        properties.setProperty("mail.smtp.starttls.enable", "true");
+        properties.setProperty("mail.debug", "true");
+
+        javaMailSender.setJavaMailProperties(properties);
+
+        return javaMailSender;
+    }
+
+}
